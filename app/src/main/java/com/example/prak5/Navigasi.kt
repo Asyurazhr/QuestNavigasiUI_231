8d8476d8389756e98b229053633c8ad778f13fa9
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-06 09:31:48 +0700

    Menambahkan fungsi DataApp dengan navController dan modifier untuk navigasi aplikasi

--- app/src/main/java/com/example/prak5/Navigasi.kt
+++ app/src/main/java/com/example/prak5/Navigasi.kt
@@ -1,11 +1,13 @@
 package com.example.prak5
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -14,4 +16,7 @@ enum class Navigasi {
 }
 
 @Composable
-
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
